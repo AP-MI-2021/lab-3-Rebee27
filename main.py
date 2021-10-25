91ea4569a216e464e418445a77c3471f670839b3
add tema lab2 <Rebee27>
2021-10-25 13:54:34 +0300

    add proprietatea 3

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ Proprietatea 3 : Verifica daca o lista este formata doar din numere prime.
 
 def printMeniu():
     print("1.Citire lista")
-    print("2.Determinare cea mai lunga subsecventa cu proprietatea 4")
-    print("3.Determinare cea mai lunga subsecventa cu proprietatea 14")
-    print("4.Determinare cea mai lunga subsecventa cu proprietatea 2")
+    print("2.Determinare cea mai lunga subsecventa cu proprietatea 1: Numerele sunt ordonate crescător")
+    print("3.Determinare cea mai lunga subsecventa cu proprietatea 2: Toate numerele au partea întreagă egală cu partea fracționară")
+    print("4.Determinare cea mai lunga subsecventa cu proprietatea 3: Verifica daca o lista este formata doar din numere prime")
     print("x.Iesire")
 
 
@@ -18,11 +18,13 @@ def citireLista():
     givenString = input("Dati lista, cu elemente separate prin virgula")
     numberAsString = givenString.split(",")
     for x in numberAsString:
-        l.append(int(x))
+        l.append(x)
     return l
 
 
 '''verifica daca lista este sortata'''
+
+
 def sorted(lst: list[int]) -> bool:
     for i in range(1, len(lst)):
         if lst[i - 1] > lst[i]:
@@ -36,6 +38,8 @@ def test_sorted():
 
 
 '''returneaza cea mai lunga subsecventa sortata ascendent'''
+
+
 def get_longest_sorted_asc(lst: list[int]) -> list[int]:
     subsecv_finala = []
     lungime_max = 0
@@ -50,33 +54,36 @@ def get_longest_sorted_asc(lst: list[int]) -> list[int]:
 
 
 '''verifica daca partea intreaga a unui numar este egala cu partea fractionara'''
-
-
 def intreaga_egal_fractionara(n: float) -> bool:
     x = str(n).split('.')
-    return x[0] == x[1]
+    if x[0] == x[1]:
+        return True
+    return False
 
 
 def test_intreaga_egal_fractionara():
-    assert intreaga_egal_fractionara(0.0) == True
+    assert intreaga_egal_fractionara(1.1) == True
     assert intreaga_egal_fractionara(78.86) == False
     assert intreaga_egal_fractionara(23.23) == True
+    assert intreaga_egal_fractionara(23.233) == False
 
 
-def is_prime(n):
-    if n < 2:
-        return False
-
-    for i in range(2, n // 2 + 1):
-        if n % i == 0:
+def lista_intreaga_egal_fractionara(lst: list[float]) -> bool:
+    for x in lst:
+        if not intreaga_egal_fractionara(x):
             return False
 
     return True
 
 
+def test_lista_intreaga_egal_fractionara():
+    assert lista_intreaga_egal_fractionara([33.33, 44.44, 56.56]) == True
+    assert lista_intreaga_egal_fractionara([]) == True
+    assert lista_intreaga_egal_fractionara([43.23, 54.89, 23.63, 85.88]) == False
+    assert lista_intreaga_egal_fractionara([1.1]) == True
+
+
 '''returneaza cea mai lunga subsecventa de numere care au partea intreaga egala cu partea fractionara'''
-
-
 def get_longest_equal_int_real(lst: list[float]) -> list[float]:
     subsecv_finala = []
     lungime_max = 0
@@ -84,16 +91,28 @@ def get_longest_equal_int_real(lst: list[float]) -> list[float]:
     for i in range(len(lst)):
         for j in range(i, len(lst)):
             subsecv_curenta = lst[i:j + 1]
-            if len(subsecv_curenta) > lungime_max and intreaga_egal_fractionara(lst):
+            if len(subsecv_curenta) > lungime_max and lista_intreaga_egal_fractionara(subsecv_curenta):
                 lungime_max = len(subsecv_curenta)
                 subsecv_finala = subsecv_curenta
     return subsecv_finala
 
 
 def test_get_longest_equal_int_real():
-    assert get_longest_equal_int_real([3.3, 58.58, 7.8, 6.6, 0.0, 36.36]) == [6.6, 0.0, 36.36]
+    assert get_longest_equal_int_real([52.52, 41.41, 21.21, 14.2, 54.54, 13.3]) == [52.52, 41.41, 21.21]
     assert get_longest_equal_int_real([3.2, 5.6, 8.9]) == []
+    assert get_longest_equal_int_real([3.2, 5.6, 8.9, 3.3]) == [3.3]
     assert get_longest_equal_int_real([]) == []
+
+
+def is_prime(n: int) -> bool:
+    if int(n) < 2:
+        return False
+
+    for i in range(2, int(n) // 2 + 1):
+        if int(n) % i == 0:
+            return False
+
+    return True
 
 
 def numere_prime(lst: list[int]) -> bool:
@@ -111,8 +130,6 @@ def test_numere_prime():
 
 
 '''returneaza cea mai lunga subsecventa a unei liste care are toate elementele prime'''
-
-
 def get_longest_all_primes(lst: list[int]) -> list[int]:
     subsecv_finala = []
     lungime_max = 0
@@ -120,16 +137,17 @@ def get_longest_all_primes(lst: list[int]) -> list[int]:
     for i in range(len(lst)):
         for j in range(i, len(lst)):
             subsecv_curenta = lst[i:j + 1]
-            if len(subsecv_curenta) > lungime_max and numere_prime(lst):
+            if len(subsecv_curenta) > lungime_max and numere_prime(subsecv_curenta):
                 lungime_max = len(subsecv_curenta)
                 subsecv_finala = subsecv_curenta
     return subsecv_finala
 
 
 def main():
-    test_numere_prime()
     test_sorted()
+    test_numere_prime()
     test_intreaga_egal_fractionara()
+    test_lista_intreaga_egal_fractionara()
     test_get_longest_equal_int_real()
     l = []
     while True:
